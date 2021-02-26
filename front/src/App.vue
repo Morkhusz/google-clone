f44d558e0927e71b275123fafcdb0950d769b5db
Jose Filho <jose-silvafilho@hotmail.com>
2021-02-26 17:01:38 -0300

    adiciona template do 'google' ao frontend

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,32 +7,67 @@
         </v-toolbar-title>
         <v-autocomplete
           rounded
-          v-model="search"
+          v-model="model"
           :items="items"
+          :item-text="`title`"
+          :item-value="`url`"
+          auto-select-first
+          no-filter
           class="mx-4"
           clearable
           clear-icon
           hide-no-data
           hide-details
           label="Start Searching"
+          :search-input.sync="search"
           solo
         ></v-autocomplete>
       </v-toolbar>
     </v-card>
-    <v-main> </v-main>
+    <v-main class="px-5 py-5">
+      <v-card class="mb-1" v-for="(item, index) in items" :key="index">
+        <v-card-text>
+          <div>{{ item.url }}</div>
+          <p class="display-1 text--primary">{{ item.title }}</p>
+          <div class="text--primary">
+            relating to or dependent on charity; charitable...
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
 
   components: {},
-  
+  created() {},
   data: () => ({
+    model: "",
     search: "",
     items: [],
   }),
+  watch: {
+    search: function (search) {
+      this.items = [];
+      axios
+        .get(`http://api.google.test/search?search=${search}`)
+        .then((res) => {
+          let results = res.data.results;
+          results.map((result) => {
+            if (!result._source.title) return;
+            this.items.push(result._source);
+          });
+        })
+        .catch((error) => {
+          error;
+        });
+    },
+  },
 };
 </script>
 <style scoped>
